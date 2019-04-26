61931c1c130a65375d3def66c8ccf3dcdff2e734
Archoss <pierrevanneste2012@gmail.com>
2019-04-26 20:03:39 +0200

    Ajout de gif pour chaque question et essai de responsive

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use('/static', express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
     res.render('index');
 });
+app.get('/gigotBitume', function (req, res) {
+    res.render('gigotBitume');
+});
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Serveur connecté sur le port 3000");

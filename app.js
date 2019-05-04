761d26bc8da8a6485ae34ba319164eed5375c0d6
Archoss <pierrevanneste2012@gmail.com>
2019-05-04 16:55:44 +0200

    Ajout de la possibilité de s'inscrire et de se connecter. Plus tard pouvoir poster des commentaires et des images

--- app.js
+++ app.js
@@ -1,17 +1,102 @@
-const express = require('express')
-const app = express()
+"use strict";
+const express = require("express"),
+    app = express(),
+    session = require("express-session"),
+    path = require("path"),
+    bodyParser = require("body-parser"),
+    expressMongoDb = require("express-mongo-db"),
+    MongoStore = require("connect-mongo")(session)
 
-app.set('view engine', 'pug');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.set("trust proxy", 1); // trust first proxy
+app.locals.pretty = true;
+
+//URL de notre base
+app.use(expressMongoDb("mongodb://pierreVanneste:bonde007FairePart@ds149676.mlab.com:49676/faire-part-db"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 app.use('/static', express.static(__dirname + '/public'));
 
+app.use(
+    session({
+        secret: "thisisasecretshhhh",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: false
+        },
+        store: new MongoStore({
+            url: "mongodb://pierreVanneste:bonde007FairePart@ds149676.mlab.com:49676/faire-part-db",
+            ttl: 30 * 60
+        })
+    })
+);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// --------------------------------------
+// --------------- ROUTES ---------------
 app.get('/', function (req, res) {
-    res.render('index');
+    const user = req.session.user;
+    const name = user.name;
+    req.db.collection('users').find().toArray((err, name) => {
+        res.render('index', {
+            name: name,
+            user: user,
+        });
+    })
+    // console.log("user :", user)
+});
+app.get("/sub", function (req, res) {
+    res.render('sub');
 });
 app.get('/gigotBitume', function (req, res) {
     res.render('gigotBitume');
 });
+app.get("/signUp", function (req, res) {
+    const user = req.session.user;
+    const name = req.session.user.name;
+    req.db.collection('users').find().toArray((err, name) => {
+        res.render('index', {
+            name: name,
+            user: user,
+        });
+    })
+    // console.log("req.session ==> ", req.session);
+});
+app.get("/signIn", function (req, res) {
+    const user = req.session.user;
+    const name = req.session.user.name;
+    req.db.collection('users').find().toArray((err, name) => {
+        res.render('index', {
+            name: name,
+            user: user,
+        });
+    })
+    console.log("req.session ==> ", req.session);
+});
+app.get("/logout", function (req, res) {
+    res.redirect("/");
+    req.session.destroy();
+});
+
+app.get("/404", function (req, res, next) {
+    res.status(404);
+    // respond with pug page
+    res.render("404");
+});
+
+// --------------------------------------
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Serveur connecté sur le port 3000");
 });
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var router = express.Router();
+// let shortId = require('short-id')
+
+/* GET home page. */
+router.get('/', function (req, res) {
+  const user = req.session.user;
+  const name = req.session.name;
+  req.db.collection('users').find().toArray((err, name) => {
+    res.render('index', {
+      name: name,
+      user: user,
+    });
+  })
+  console.log("req.session ==> ", req.session);
+})
+  .post('/signUp', function (req, res) {
+    // Nous utilisons le schéma Piscine
+    let user = {
+      name: req.body.pseudo.trim(),
+      password: req.body.password
+    }
+    // /---/  MONGO  /---/ //
+    req.db.collection('users').findOne({
+      "name": {
+        $regex: user.name,
+        $options: "is"
+      },
+      "password": {
+        $regex: user.password,
+        $options: "is"
+      },
+    }, function (err, result) {
+      if (result) {
+        console.log("result --->", result)
+        console.log("Le pseudo et/ou le mot de passe est déja pris")
+        res.render('signUp', {
+          error: true,
+          message: "Le pseudo et/ou le mot de passe est déja pris"
+        })
+      } else {
+        // bcrypt.hash(req.body.password, 10, function (err, hash) {
+        //   // Store hash in database
+        // });
+        req.db.collection('users').insertOne(user,
+          err => {
+            if (err) {
+              throw err;
+            } else {
+              req.session.user = user;
+              res.render("index", {
+                user: user
+              });
+            }
+          })
+      }
+    })
+    console.log("req.session ==> ", req.session);
+    console.log('--- --- Profil ajouté à la base de données--- ---')
+  })
+  .post('/signIn', function (req, res) {
+    const {
+      name,
+      password
+    } = {
+      name: req.body.pseudo.trim(),
+      password: req.body.password
+    };
+    // /---/  MONGO  /---/ //
+    req.db.collection('users').findOne({
+      name,
+      password
+    }, function (err, user) {
+      if (user) {
+        console.log("Connexion")
+        req.session.user = user;
+        res.render("index", {
+          user: user,
+        });
+        // bcrypt.hash(req.body.password, 10, function (err, hash) {
+        //   // Store hash in database
+        // });
+      }
+    })
+    console.log('--- --- Connexion --- ---')
+  })
+
+module.exports = router;
